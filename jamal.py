c181eb908dfc489270ac43a121d1c6699489411c
Vicky-Sam <126162764+Vicky-Sam@users.noreply.github.com>
2025-08-20 13:07:42 +0530

    Create jamal.py

---
+++ jamal.py
@@ -0,0 +1,9 @@
+dfkndsklf
+dkfndlf
+k,dnfd
+
+[default]
+aws_access_key_id = AKIA2T2SJH6M6IK3TTM7
+aws_secret_access_key = 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
+output = json
+region = us-east-2
