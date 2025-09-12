dce3c64b8ac16b531954fe04555a137577c59cf2
Vicky-Sam <126162764+Vicky-Sam@users.noreply.github.com>
2025-09-12 09:38:50 +0530

    Create leo.py

---
+++ leo.py
@@ -0,0 +1,11 @@
+[default]
+aws_access_key_id = AKIA2T2SJH6M5P6IQVJI
+aws_secret_access_key = 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
+output = json
+region = us-east-2
+df'dk'
+dgf
+d'lg
+
+sdg
+sdgsdg
