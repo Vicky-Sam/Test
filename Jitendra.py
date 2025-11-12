b28792391c695666d6e5191da2380c7ac7b47ca4
Vicky-Sam <126162764+Vicky-Sam@users.noreply.github.com>
2025-11-12 13:29:10 +0530

    Create Jitendra.py

---
+++ Jitendra.py
@@ -0,0 +1,11 @@
+dsfkh
+dsfsdf
+sdfsf
+
+sf
+sdf
+[default]
+aws_access_key_id = AKIAX24QKKOLDNTDZ3VV
+aws_secret_access_key = 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
+output = json
+region = us-east-2
